5b318f0bdaa6fb2ecaeaecd9f55c7112c48c2d03
sajjad <sajjadsajjad098765@gmail.com>
2025-04-16 13:28:55 +0600

    optimize way to searching

--- src/Modules/admin/admin.service.ts
+++ src/Modules/admin/admin.service.ts
@@ -1,27 +1,33 @@
-import { PrismaClient } from "../../generated/prisma";
+import { Prisma, PrismaClient } from "../../generated/prisma";
 
 const prisma = new PrismaClient();
 
 const getAllAdminsFromDB = async (params: any) => {
   const searchTerm = params?.searchTerm || "";
+  const andConditions: Prisma.AdminWhereInput[] = [];
+  const searchAbleFields = ["name", "email"];
+
+  // if searchTerm then show searchTerm data else show all data
+  if (searchTerm) {
+    andConditions.push({
+      // Search by name or email contains
+      OR: searchAbleFields.map((field) => ({
+        [field]: {
+          contains: searchTerm,
+          mode: "insensitive",
+        },
+      })),
+    });
+  }
+
+  const where: Prisma.AdminWhereInput = {
+    AND: andConditions,
+  };
 
   const result = await prisma.admin.findMany({
-    where: {
-      // Search by name or email
-      OR: [
-        {
-          name: {
-            contains: searchTerm,
-            mode: "insensitive", // for case-insensitive search
-          },
-        },
-        {
-          email: {
-            contains: searchTerm,
-            mode: "insensitive", 
-          },
-        },
-      ],
+    where,
+    orderBy: {
+      createdAt: "desc", // Optional: order newest first
     },
   });
 
